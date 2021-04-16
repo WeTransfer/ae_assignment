b455048136cee68b29218ee56f0bd724eaedb079
Rafael Barbosa <rafael.barbosa@wetransfer.com>
2021-04-16 16:10:02 +0200

    simplify column names

--- models/raw/raw_events.sql
+++ models/raw/raw_events.sql
@@ -1,10 +1,10 @@
 select
-  sg_event_id,
+  sg_event_id as event_id,
   event,
   email,
   "timestamp",
-  marketing_campaign_id,
-  marketing_campaign_split_id,
-  marketing_campaign_version,
-  marketing_campaign_name
+  marketing_campaign_id as campaign_id,
+  marketing_campaign_split_id as campaign_split_id,
+  marketing_campaign_version as campaign_version,
+  marketing_campaign_name as campaign_name
 from {{ ref('sample_events')}}
